9af1b331709114a3baa17775aaa54594da2f67a3
Cristian Sanchez <cristian.manu7@gmail.com>
2022-12-18 14:09:54 -0700

    mapping

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import logo from './logo.svg'
+
 import './App.css'
 
 // class Component
@@ -9,37 +9,26 @@ class App extends Component {
     super()
 
     this.state = {
-      name: { fristName: 'Cristian', lastName: 'Zhang' },
-      company: 'ztm'
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jacky'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.fristName} {this.state.name.lastName} , I work
-            at {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: 'James', lastName: 'Gomez' }
-                  }
-                },
-                () => {
-                  console.log(this.state)
-                }
-              )
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>
+        })}
       </div>
     )
   }

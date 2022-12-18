d3845da05d525499c46007127c76181b8fa7279f
Cristian Sanchez <cristian.manu7@gmail.com>
2022-12-18 13:32:47 -0700

    setSate

--- src/App.js
+++ src/App.js
@@ -19,7 +19,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi {this.state.name}</p>
-          <button>Change Name</button>
+          <button
+            onClick={() => {
+              this.setState({ name: 'James' })
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     )
